bcc47c12c752a9c4f6d5387ad93f682e3e9731ff
Sheenan Tenepre <sheenantenepre@gmail.com>
2016-12-07 00:44:20 +0900

    add eslint features

---
+++ src/__test__/ActivityComponent.spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ActivityComponent } from 'components'
+import { shallow } from 'enzyme'
+
+describe('Sample test a component', () => {
+
+  it ('Component exists', () => {
+    const activtyCompo = shallow(<ActivityComponent />)
+    expect(activtyCompo.find(".Activity")).to.have.length(1)
+
+  })
+
+})
