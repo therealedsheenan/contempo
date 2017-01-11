a78f64c0e4e917a4fa3687c0e5ea8f60d81d0098
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-01-11 11:44:13 +0900

    update name function

--- src/client/redux/modules/index.js
+++ src/client/redux/modules/index.js
@@ -4,7 +4,7 @@ const initialState = {
   greeting: ''
 }
 
-export default function greeting (state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case INITIATE_GREETING :
       return {
