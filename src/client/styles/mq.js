8ea8eead5afca878f992309d17d4e30db668c61c
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-01-29 14:56:15 +0900

    updating style functionalities and add styled-components

---
+++ src/client/styles/mq.js
@@ -0,0 +1,15 @@
+// partial sizing for media queries
+
+const mobile = '(min-width: 320px)'
+const mobileOnly = '(min-width: 320px) and (max-width: 480px)'
+const tablet = '(min-width: 481px)'
+const tabletOnly = '(min-width: 481px) and (max-width: 767px)'
+const desktop = '(min-width: 768px)'
+
+export default {
+  mobile,
+  mobileOnly,
+  tablet,
+  tabletOnly,
+  desktop
+}
---
+++ src/client/styles/styledMq.js
@@ -0,0 +1,26 @@
+import mq from 'styles/mq'
+import { css } from 'styled-components'
+
+export const styledMobile = {
+  handheld: (...args) => css`
+    @media ${mq.mobile} {
+      ${ css(...args) }
+    }
+  `
+}
+
+export const styledTablet = {
+  handheld: (...args) => css`
+    @media ${mq.tablet} {
+      ${ css(...args) }
+    }
+  `
+}
+
+export const styledDesktop = {
+  handheld: (...args) => css`
+    @media ${mq.desktop} {
+      ${ css(...args) }
+    }
+  `
+}
