2640c6c6a732e077ff3229ca738ffad2554fc5c1
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-03-14 23:06:40 +0900

    move test to component directory

---
+++ src/client/components/Greeting/__test__/GreetingComponet.spec.js
@@ -0,0 +1,9 @@
+// Link.react-test.js
+import React from 'react'
+import renderer from 'react-test-renderer'
+import GreetingComponent from '../GreetingComponent'
+
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(<GreetingComponent />).toJSON()
+  expect(component).toMatchSnapshot()
+})
