034d0ab02f6d2834fdf6449ddd2345f7f9735c20
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-04-18 19:05:57 +0900

    async routes

--- src/client/config/routes.js
+++ src/client/config/routes.js
@@ -4,19 +4,17 @@ import { Route } from 'react-router-dom'
 // react containers
 import Root from '../components/Root'
 import AsyncRoute from '../components/AsyncRoute/AsyncRoute'
-import HomeContainer from '../containers/Home/HomeContainer'
 
 const routes = () => (
   <Root>
-    <Route exact path='/' component={HomeContainer} />
 
-    {/*<Route strict exact path='/' render={props => (*/}
-      {/*<AsyncRoute*/}
-        {/*props={props}*/}
-        {/*loadingPromise={*/}
-          {/*System.import('../containers/Home/HomeContainer')*/}
-        {/*} />*/}
-    {/*)} />*/}
+    <Route strict exact path='/' render={props => (
+      <AsyncRoute
+        props={props}
+        loadingPromise={
+          System.import('../containers/Home/HomeContainer')
+        } />
+    )} />
 
     <Route strict exact path='/style' render={props => (
       <AsyncRoute
