b01eaffd723cd4892e971a0ec814dee489148771
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-04-18 19:03:24 +0900

    modernize

--- src/client/components/Greeting/GreetingComponent.js
+++ src/client/components/Greeting/GreetingComponent.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Motion, spring} from 'react-motion'
 import PropTypes from 'prop-types'
 
 import {
@@ -9,14 +8,10 @@ import {
 
 const GreetingComponent = (props) => {
   return (
-    <Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
-      {val =>
-        <div className='Greeting'>
-          <Title>Good {props.message}!</Title>
-          <SubTitle>Welcome to contempo!</SubTitle>
-        </div>
-      }
-    </Motion>
+    <div className='Greeting'>
+      <Title>Good {props.message}!</Title>
+      <SubTitle>Welcome to contempo!</SubTitle>
+    </div>
   )
 }
 
--- src/client/config/routes.js
+++ src/client/config/routes.js
@@ -4,16 +4,19 @@ import { Route } from 'react-router-dom'
 // react containers
 import Root from '../components/Root'
 import AsyncRoute from '../components/AsyncRoute/AsyncRoute'
+import HomeContainer from '../containers/Home/HomeContainer'
 
 const routes = () => (
   <Root>
-    <Route strict exact path='/' render={props => (
-      <AsyncRoute
-        props={props}
-        loadingPromise={
-          System.import('../containers/Home/HomeContainer')
-        } />
-    )} />
+    <Route exact path='/' component={HomeContainer} />
+
+    {/*<Route strict exact path='/' render={props => (*/}
+      {/*<AsyncRoute*/}
+        {/*props={props}*/}
+        {/*loadingPromise={*/}
+          {/*System.import('../containers/Home/HomeContainer')*/}
+        {/*} />*/}
+    {/*)} />*/}
 
     <Route strict exact path='/style' render={props => (
       <AsyncRoute
