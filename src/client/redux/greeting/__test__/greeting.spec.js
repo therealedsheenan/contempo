c520782903badb7da56fb9199f1117a0ac65014f
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-04-21 16:28:32 +0900

    redux eslint

--- src/client/redux/greeting/__test__/greeting.spec.js
+++ src/client/redux/greeting/__test__/greeting.spec.js
@@ -1,51 +1,47 @@
-import 'rxjs'
-import nock from 'nock'
-import configureMockStore from 'redux-mock-store'
-import { createEpicMiddleware, combineEpics } from 'redux-observable'
+import 'rxjs';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import { createEpicMiddleware, combineEpics } from 'redux-observable';
 
 // modules
 import {
   requestGreeting,
   greetingEpic
-} from '../actions'
+} from '../actions';
 
-import greetingReducer from '../reducer'
-import * as type from '../types'
+import greetingReducer from '../reducer';
+import * as type from '../types';
 
-const combinedEpics = combineEpics(
-  greetingEpic
-)
+const combinedEpics = combineEpics(greetingEpic);
 
-const epicMiddleware = createEpicMiddleware(combinedEpics)
-const mockStore = configureMockStore([epicMiddleware])
+const epicMiddleware = createEpicMiddleware(combinedEpics);
+const mockStore = configureMockStore([epicMiddleware]);
 
 const initialState = {
   greeting: '',
   fetching: true
-}
+};
 
 describe('Greeting Actions', () => {
   it('Should create action to request the greeting', () => {
     const expectedAction = {
       type: type.GET_GREETING
-    }
-    expect(
-      requestGreeting()
-    )
-    .toEqual(expectedAction)
-  })
-})
+    };
+    expect(requestGreeting())
+    .toEqual(expectedAction);
+  });
+});
 
 describe('Greeting reducer', () => {
-  let store
+  let store;
   beforeEach(() => {
-    store = mockStore()
-  })
+    store = mockStore();
+  });
 
   afterEach(() => {
-    nock.cleanAll()
-    epicMiddleware.replaceEpic(greetingEpic)
-  })
+    nock.cleanAll();
+    epicMiddleware.replaceEpic(greetingEpic);
+  });
 
   it('Should show the initial state', () => {
     expect(
@@ -53,41 +49,34 @@ describe('Greeting reducer', () => {
         undefined,
         {}
       )
-    ).toEqual(initialState)
-  })
+    ).toEqual(initialState);
+  });
 
   it('Should show request a greeting', () => {
     expect(
       greetingReducer(
         initialState,
-        requestGreeting()
-      )
-    )
-    .toEqual(
-      {
-        fetching: true,
-        greeting: ''
-      }
-    )
-  })
+        requestGreeting())).toEqual({
+          fetching: true,
+          greeting: ''
+        });
+  });
 
   it('Should show the successful greeting', () => {
-    let mockGreeting = {
+    const mockGreeting = {
       type: type.GET_GREETING_SUCCESS,
       greeting: {
         type: 1,
         content: 'Evening'
       }
-    }
+    };
 
-    store.dispatch(requestGreeting())
-    store.dispatch(mockGreeting)
+    store.dispatch(requestGreeting());
+    store.dispatch(mockGreeting);
 
-    expect(store.getActions()).toEqual(
-      [
-        requestGreeting(),
-        mockGreeting
-      ]
-    )
-  })
-})
+    expect(store.getActions()).toEqual([
+      requestGreeting(),
+      mockGreeting
+    ]);
+  });
+});
