dc8a711172c47290c7aa8a9cd9b32c087bd6734c
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-02 15:36:24 +0900

    async components

--- src/client/components/Greeting/GreetingComponent.jsx
+++ src/client/components/Greeting/GreetingComponent.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Test usage of Async Component
+import Async from '../Async/AsyncComponent';
+
 import {
   Title,
   SubTitle,
   Wrapper
 } from './styles';
 
+
 const GreetingComponent = props => (
   <Wrapper>
     <Title>Good {props.message}!</Title>
     <SubTitle>Hello there! Welcome to contempo.</SubTitle>
-    <img src="./assets/images/sample.jpg" alt="" />
+    <img src="./assets/images/contempo.png" alt="" />
   </Wrapper>
   );
 
