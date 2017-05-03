bddac22b4b2d08165fd93f7ff12b9819d38ed877
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-03 23:26:10 +0900

    add favicon task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,12 +12,19 @@ gulp.task('copy-assets', () => {
     .pipe(gulp.dest('public/assets'));
 });
 
+// copying favicon to public directory
+gulp.task('copy-favicon', () => {
+  gulp.src('./favicon.ico')
+    .pipe(gulp.dest('public'));
+});
+
 // running default tasks
-gulp.task('default', ['clean-assets', 'copy-assets'], () => {
+gulp.task('default', ['clean-assets', 'copy-favicon', 'copy-assets'], () => {
   gulp.watch([
     './src/client/assets/**'
   ], () => {
     gulp.run('clean-assets');
+    gulp.run('copy-favicon');
     gulp.run('copy-assets');
   });
 });
