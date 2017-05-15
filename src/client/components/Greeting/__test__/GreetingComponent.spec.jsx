f85f2703743525b2e59384fe480575e1aa4dd081
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-15 16:07:59 +0900

    update dependency

--- src/client/components/Greeting/__test__/GreetingComponent.spec.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import GreetingComponent from '../GreetingComponent';
-
-test('Link changes the class when hovered', () => {
-  const component = renderer.create(<GreetingComponent />).toJSON();
-  expect(component).toMatchSnapshot();
-});
