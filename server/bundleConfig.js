b4c9d677b155e3b95b1f0ec7d03a21482d048dbb
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-26 12:41:33 +0900

    new webpack config on server-side rendering

---
+++ server/bundleConfig.js
@@ -0,0 +1,48 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const OfflinePlugin = require('offline-plugin');
+
+// View the bundle-analyzer plugin by uncommenting the next line.
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const CONFIG = {
+  app: resolve('src'),
+  output: resolve(__dirname, './public'),
+  entry: './server/index.jsx',
+  port: 8000
+};
+
+module.exports = function (type) {
+  const bundleConfig = {
+    context: resolve(__dirname, '..'),
+    entry: [],
+    plugins: []
+  };
+
+  if (type === 'DEV') {
+    bundleConfig.entry = [CONFIG.entry];
+    bundleConfig.devtool = 'inline-source-map';
+  } else {
+    bundleConfig.entry = [CONFIG.entry];
+    bundleConfig.devtool = 'cheap-source-map';
+    bundleConfig.plugins.push(
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        sourceMap: true
+      }),
+      new OfflinePlugin()
+    );
+  }
+  return bundleConfig;
+};
