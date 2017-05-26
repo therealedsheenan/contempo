8b3701c86e7651b408dd9105366f626a77f69b13
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-26 16:36:29 +0900

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,10 @@ const bundleConfig = require('./common');
 const clientConfig = require('./client/bundleConfig');
 const serverConfig = require('./server/bundleConfig');
 
+// tasks configs
+const copyConfig = require('./tools/copy');
+const cleanConfig = require('./tools/clean');
+
 
 const CONFIG = {
   output: resolve(__dirname, './public'),
@@ -15,40 +19,60 @@ const CONFIG = {
 
 module.exports = function (grunt) {
   const TARGET = grunt.option('target');
+
   if (TARGET === 'client') {
-    grunt.initConfig({
-      webpack: {
-        prod: Merge(bundleConfig, clientConfig('PROD'))
-      },
-      'webpack-dev-server': {
-        options: {
-          webpack: Merge(bundleConfig, clientConfig('DEV')),
-          port: CONFIG.port,
-          historyApiFallback: true,
-          stats: 'errors-only'
-        },
-        start: {}
-      }
-    });
+    /* SERVER-SIDE RENDERING CONDFIGURATIONS */
+
+    grunt.initConfig(
+      Object.assign(
+        copyConfig,
+        cleanConfig, {
+          webpack: {
+            prod: Merge(bundleConfig, clientConfig('PROD'))
+          },
+          'webpack-dev-server': {
+            options: {
+              webpack: Merge(bundleConfig, clientConfig('DEV')),
+              port: CONFIG.port,
+              historyApiFallback: true,
+              stats: 'errors-only',
+              contentBase: CONFIG.output
+            },
+            start: {}
+          }
+        }
+      )
+    );
     // load tasks
     grunt.loadNpmTasks('grunt-webpack');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // register tasks
-    grunt.registerTask('default', ['webpack-dev-server']);
-    grunt.registerTask('prod', ['webpack-dev-server']);
+    grunt.registerTask('default', ['clean', 'copy', 'webpack-dev-server']);
+    grunt.registerTask('prod', ['clean', 'copy', 'webpack-dev-server']);
   } else {
-    grunt.initConfig({
-      webpack: {
-        prod: Merge(bundleConfig, serverConfig('PROD')),
-        dev: Object.assign({ watch: true, cache: true }, Merge(bundleConfig, serverConfig('DEV')))
-      }
-    });
+    /* CLIENT-SIDE RENDERING CONDFIGURATIONS */
+
+    grunt.initConfig(
+      Object.assign(
+        copyConfig,
+        cleanConfig, {
+          webpack: {
+            prod: Merge(bundleConfig, serverConfig('PROD')),
+            dev: Object.assign({ watch: true, cache: true }, Merge(bundleConfig, serverConfig('DEV')))
+          }
+        }
+      )
+    );
 
     // register tasks
     grunt.loadNpmTasks('grunt-webpack');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // register tasks
-    grunt.registerTask('default', ['webpack:dev']);
-    grunt.registerTask('prod', ['webpack:prod']);
+    grunt.registerTask('default', ['clean', 'copy', 'webpack:dev']);
+    grunt.registerTask('prod', ['clean', 'copy', 'webpack:prod']);
   }
 };
