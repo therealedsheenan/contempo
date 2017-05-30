2ed084a466fdbc989d1dca382015808c5ed61444
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-30 13:35:36 +0900

    simplify gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,18 @@
-// const webpackConfig = require('./webpack.config');
-const { resolve } = require('path');
-const Merge = require('webpack-merge');
-
-// dependencies
-const bundleConfig = require('./webpack');
-const clientConfig = require('./client/bundleConfig');
-const serverConfig = require('./server/bundleConfig');
-
 // tasks configs
 const copyConfig = require('./tools/copy');
 const cleanConfig = require('./tools/clean');
 
-const CONFIG = {
-  output: resolve(__dirname, './public'),
-  port: 8000
-};
-
 module.exports = function gruntFile(grunt) {
-  const TARGET = grunt.option('target');
+  grunt.initConfig(
+    Object.assign(
+      copyConfig,
+      cleanConfig
+    )
+  );
+  // load tasks
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  if (TARGET === 'client') {
-    /* CLIENT-SIDE RENDERING CONFIGURATIONS */
-
-    grunt.initConfig(
-      Object.assign(
-        copyConfig,
-        cleanConfig, {
-          webpack: {
-            prod: Merge(bundleConfig, clientConfig('PROD'))
-          },
-          'webpack-dev-server': {
-            options: {
-              webpack: Merge(bundleConfig, clientConfig('DEV')),
-              port: CONFIG.port,
-              historyApiFallback: true,
-              stats: 'errors-only',
-              contentBase: CONFIG.output
-            },
-            start: {}
-          }
-        }
-      )
-    );
-    // load tasks
-    grunt.loadNpmTasks('grunt-webpack');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
-    // register tasks
-    grunt.registerTask('default', ['clean', 'copy', 'webpack-dev-server']);
-    grunt.registerTask('prod', ['clean', 'copy', 'webpack:prod']);
-  } else if (TARGET === 'server') {
-    /* SERVER-SIDE RENDERING CONDFIGURATIONS */
-
-    grunt.initConfig(
-      Object.assign(
-        copyConfig,
-        cleanConfig, {
-          webpack: {
-            prod: Merge(bundleConfig, serverConfig('PROD')),
-            dev: Object.assign({ watch: true, cache: true }, Merge(bundleConfig, serverConfig('DEV')))
-          }
-        }
-      )
-    );
-
-    // register tasks
-    grunt.loadNpmTasks('grunt-webpack');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
-    // register tasks
-    grunt.registerTask('default', ['clean', 'copy', 'webpack:dev']);
-    grunt.registerTask('prod', ['clean', 'copy', 'webpack:prod']);
-  } else {
-    // static clean and copy tasks
-    grunt.initConfig(
-      Object.assign(
-        {},
-        cleanConfig,
-        copyConfig
-      )
-    );
-
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-
-    // register tasks
-    grunt.registerTask('assets', ['clean', 'copy']);
-  }
+  // register tasks
+  grunt.registerTask('default', ['clean', 'copy']);
 };
