71efeb3a292dc03ae2ae1c1edb7935efd55740d8
therealedsheenan <private.sheenantenepre@gmail.com>
2017-11-20 14:51:31 +0900

    client deploy scripts

--- deploy-client.js
+++ deploy-client.js
@@ -2,17 +2,16 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 
 // serve static static normally
-// static files
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/static',  express.static(path.join(__dirname, 'public/static')));
+// asset files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Handles all routes so you do not get a not found error
-app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, 'public/static', 'index.html'));
+app.get('/*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port);
