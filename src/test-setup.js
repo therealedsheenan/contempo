b2e8198d1c3e25924594dc8134eed7404b78452c
therealedsheenan <private.sheenantenepre@gmail.com>
2017-11-27 14:05:21 +0900

    testing setup

---
+++ src/test-setup.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
+
+/* eslint-disable */
---
+++ src/test-utils/mockApp.js
@@ -0,0 +1,24 @@
+
+// @flow
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { mount } from 'enzyme';
+
+import AppRoutes from '../config/routes';
+import createStore from '../config/store';
+
+export default () => {
+  const store = createStore;
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  const wrapper = mount(
+    <Provider store={store}>
+      <Router history={history}>
+        <AppRoutes />
+      </Router>
+    </Provider>
+  );
+  return { history, store, wrapper };
+}
